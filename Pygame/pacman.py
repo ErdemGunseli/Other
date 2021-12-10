3e5b6ec36b1263c47dfe63a02f7eb478a8f6ebbb
Erdem GUNSELI <Egunseli4@gmail.com>
2021-12-10 22:35:52 +0000

    Update pacman.py

--- Pygame/pacman.py
+++ Pygame/pacman.py
@@ -9,33 +9,51 @@ YELLOW = (255,255,0)
 
 pygame.init()
 
-#Blank Screen:
-size = (640,480)
+map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+
+mapXSize = len(map[0])
+mapYSize = len(map)
+
+# The number of pixels that make up a tile.
+pixelPerTile = 40
+
+# The size of the screen should be number of tiles * number of pixels per tile.
+size = (mapXSize * pixelPerTile, mapYSize * pixelPerTile)
 screen = pygame.display.set_mode(size)
 
+# Sprite Groups:
 allSpritesGroup = pygame.sprite.Group()
 wallGroup = pygame.sprite.Group()
 playerGroup = pygame.sprite.Group()
 
 
-map = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        ]
 
-mapXLength = len(map[0])
-mapYLength = len(map)
 
-screenXLength = size[0]
-screenYLength = size[1]
+
+
+screenXSize = size[0]
+screenYSize = size[1]
 
 
 
@@ -52,10 +70,6 @@ class Tile(pygame.sprite.Sprite):
         self.rect.y = yPos
 
 
-        self.speed = 0
-        self.xSpeed = self.speed
-        self.ySpeed = self.speed
-
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, color, width, height, xPos, yPos):
@@ -69,107 +83,67 @@ class Player(pygame.sprite.Sprite):
         self.rect.x = xPos
         self.rect.y = yPos
 
-    def playerInput(self):
-
-        # User Inputs:
-        # Since the player can move up and down as well, the arguments include which axis to move the player in.
+    def getPlayerInput(self):
+        # Passes the axis and direction of the movement depending on the key pressed.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quit = True
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    self.setPlayerSpeed("x",-5)
+                    self.movePlayer("x", -1)
                 if event.key == pygame.K_d:
-                    self.setPlayerSpeed("x",5)
+                    self.movePlayer("x", 1)
                 if event.key == pygame.K_w:
-                    self.setPlayerSpeed("y",-5)
+                    self.movePlayer("y", -1)
                 if event.key == pygame.K_s:
-                    self.setPlayerSpeed("y",5)
-                if event.key == pygame.K_SPACE:
-                    self.bulletShooter()
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_a or event.key == pygame.K_d:
-                    self.setPlayerSpeed("x",0)
-                elif event.key == pygame.K_w or event.key == pygame.K_s:
-                    self.setPlayerSpeed("y",0)
-
-        # Ensures that the player cannot leave the screen:
-        if self.rect.x < 0: self.rect.x = 0
-        elif self.rect.x > 630: self.rect.x = 630
-
-        if self.rect.y < 0: self.rect.x = 0
-     
-        elif self.rect.y > 470: self.rect.y = 470
-
+                    self.movePlayer("y", 1)
     
-    def setPlayerSpeed(self, coordinate, speed):
-        # Sets the player's speed, the direction of which is determined by the argument passed.
-        if coordinate == "x":
-            self.xSpeed = speed
-        elif coordinate == "y":
-            self.ySpeed = speed
+    def movePlayer(self, axis, vector):
+        # Moves the player 1 tile in the given direction.
+        if axis == "x":
+            self.rect.x += vector * pixelPerTile
+        elif axis == "y":
+            self.rect.y += vector * pixelPerTile
 
+            # TODO: Do not change the movement until collisions are done. After that, make the movement work as intended.
     
     def update(self):
-
-        # Gets the player input.
-        self.playerInput()
-        self.rect.x += self.xSpeed
-        self.rect.y += self.ySpeed
+        self.getPlayerInput()
 
 
 
-for x in range(len(map[0])):
-        for y in range(len(map)):
+for x in range(mapXSize):
+        for y in range(mapYSize):
             if map[x][y] == 1:
                 # Assuming the screen is landscape, the following will cause the square map to fill the height of the screen and be centered on the width.
-                myWall = Tile(BLUE, (screenYLength / mapXLength), (size[1] / mapYLength), (x * screenYLength / mapXLength) + ((screenXLength - screenYLength) / 2), (y * screenYLength / mapYLength))
+                myWall = Tile(BLUE, (screenYSize / mapXSize), (size[1] / mapYSize), (x * screenYSize / mapXSize) + ((screenXSize - screenYSize) / 2), (y * screenYSize / mapYSize))
                 wallGroup.add(myWall)
                 allSpritesGroup.add(myWall)
 
-# Creates the player that is 1-tile sized
-myPlayer = Player(YELLOW, (screenYLength / 10),(screenYLength / 10), 200, 200,)
+# Creates the player that is 1-tile sized, at the top left corner:
+myPlayer = Player(YELLOW, pixelPerTile, pixelPerTile, pixelPerTile, pixelPerTile)
 playerGroup.add(myPlayer)
 allSpritesGroup.add(myPlayer)
 
-
-
 #The title of the new window:
-pygame.display.set_caption("My Window")
+pygame.display.set_caption("PacMan")
 
 #When this is true, the game will end:
 done = False
 
 clock = pygame.time.Clock()
 
-### This is the Game Loop:
-
 while not done:
 
-    #User inputs and controls:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        #endif
-    #Next
-
-    ##The Game Logic:
-
-    #The screen background is BLACK:
     screen.fill(BLACK)
-        
-    
 
-    allSpritesGroup.update()
+    playerGroup.update()
     allSpritesGroup.draw(screen)
 
-    #This will flip the display to reveal the new position of objects:
-    pygame.display.flip()
 
-    #The clock ticks over:
+    pygame.display.flip()
     clock.tick(60)
 
-#endwhile - The end of the game loop.
 
 pygame.quit()
 
