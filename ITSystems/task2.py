b52329e9e13b6101dbe140f4a1dcb1dbc02be94e
Erdem <egunseli4@gmail.com>
2022-03-17 22:41:00 +0000

    updates

--- ITSystems/task2.py
+++ ITSystems/task2.py
@@ -1,4 +1,3 @@
-
 class User():
 
     def __init__(self, fName, lName, email, password):
@@ -16,13 +15,12 @@ class User():
     def validPassword(self):
         password = self.password
 
-        if len(password) >= 0 and len(password) <= 9 and password != password.upper() and password != password.lower():
-                for i in range(10):
-                    if str(i) in password:
-                        return True        
+        if len(password) >= 9 and len(password) <= 12 and password != password.upper() and password != password.lower():
+                for i in range(10): 
+                    if str(i) in password: return True        
         return False      
 
-
+## TESTING:
 
 myUser =  User("Bob", "Dave", "bobdave@gmail.com", "Password1")
 
@@ -36,3 +34,6 @@ if myUser.validPassword():
     print("Password Valid")
 else:
     print("Password Invalid")           
+
+# Password needs uppercase, lowercase, number, length between 9, 12 inclusive.
+# Email needs @ sign and .
