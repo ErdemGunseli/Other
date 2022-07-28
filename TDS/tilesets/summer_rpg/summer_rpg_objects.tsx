a0e80a4ca0caaa8ede3ddf9af323a44d75217c7f
eguns <egunseli4@gmail.com>
2022-07-28 05:27:42 +0100

    Backup

---
+++ TDS/tilesets/summer_rpg/summer_rpg_objects.tsx
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="summer_rpg_objects" tilewidth="256" tileheight="256" tilecount="39" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="234" source="summer_rpg_objects/tree_15.png"/>
+ </tile>
+ <tile id="1">
+  <image width="168" height="256" source="summer_rpg_objects/building_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="151" height="256" source="summer_rpg_objects/building_2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="244" height="256" source="summer_rpg_objects/building_3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="89" height="256" source="summer_rpg_objects/building_4.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="246" source="summer_rpg_objects/building_5.png"/>
+ </tile>
+ <tile id="6">
+  <image width="123" height="256" source="summer_rpg_objects/building_6.png"/>
+ </tile>
+ <tile id="7">
+  <image width="184" height="256" source="summer_rpg_objects/building_7.png"/>
+ </tile>
+ <tile id="8">
+  <image width="180" height="256" source="summer_rpg_objects/decor_1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="145" height="256" source="summer_rpg_objects/decor_2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="239" source="summer_rpg_objects/decor_3.png"/>
+ </tile>
+ <tile id="11">
+  <image width="70" height="256" source="summer_rpg_objects/decor_4.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="89" source="summer_rpg_objects/decor_5.png"/>
+ </tile>
+ <tile id="13">
+  <image width="256" height="56" source="summer_rpg_objects/decor_6.png"/>
+ </tile>
+ <tile id="14">
+  <image width="256" height="82" source="summer_rpg_objects/decor_7.png"/>
+ </tile>
+ <tile id="15">
+  <image width="187" height="256" source="summer_rpg_objects/decor_8.png"/>
+ </tile>
+ <tile id="16">
+  <image width="202" height="256" source="summer_rpg_objects/decor_9.png"/>
+ </tile>
+ <tile id="17">
+  <image width="227" height="256" source="summer_rpg_objects/decor_10.png"/>
+ </tile>
+ <tile id="18">
+  <image width="173" height="256" source="summer_rpg_objects/decor_11.png"/>
+ </tile>
+ <tile id="19">
+  <image width="235" height="256" source="summer_rpg_objects/decor_12.png"/>
+ </tile>
+ <tile id="20">
+  <image width="256" height="182" source="summer_rpg_objects/decor_13.png"/>
+ </tile>
+ <tile id="21">
+  <image width="177" height="256" source="summer_rpg_objects/decor_14.png"/>
+ </tile>
+ <tile id="22">
+  <image width="223" height="256" source="summer_rpg_objects/decor_16.png"/>
+ </tile>
+ <tile id="23">
+  <image width="237" height="256" source="summer_rpg_objects/stones_1.png"/>
+ </tile>
+ <tile id="24">
+  <image width="256" height="201" source="summer_rpg_objects/stones_4.png"/>
+ </tile>
+ <tile id="25">
+  <image width="157" height="256" source="summer_rpg_objects/tree_1.png"/>
+ </tile>
+ <tile id="26">
+  <image width="157" height="256" source="summer_rpg_objects/tree_2.png"/>
+ </tile>
+ <tile id="27">
+  <image width="147" height="256" source="summer_rpg_objects/tree_3.png"/>
+ </tile>
+ <tile id="28">
+  <image width="175" height="256" source="summer_rpg_objects/tree_4.png"/>
+ </tile>
+ <tile id="29">
+  <image width="186" height="256" source="summer_rpg_objects/tree_5.png"/>
+ </tile>
+ <tile id="30">
+  <image width="211" height="256" source="summer_rpg_objects/tree_6.png"/>
+ </tile>
+ <tile id="31">
+  <image width="153" height="256" source="summer_rpg_objects/tree_7.png"/>
+ </tile>
+ <tile id="32">
+  <image width="143" height="256" source="summer_rpg_objects/tree_8.png"/>
+ </tile>
+ <tile id="33">
+  <image width="147" height="256" source="summer_rpg_objects/tree_9.png"/>
+ </tile>
+ <tile id="34">
+  <image width="211" height="256" source="summer_rpg_objects/tree_10.png"/>
+ </tile>
+ <tile id="35">
+  <image width="154" height="256" source="summer_rpg_objects/tree_11.png"/>
+ </tile>
+ <tile id="36">
+  <image width="169" height="256" source="summer_rpg_objects/tree_12.png"/>
+ </tile>
+ <tile id="37">
+  <image width="256" height="199" source="summer_rpg_objects/tree_13.png"/>
+ </tile>
+ <tile id="38">
+  <image width="256" height="172" source="summer_rpg_objects/tree_14.png"/>
+ </tile>
+</tileset>
